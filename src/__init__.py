9edeaf541f94c6307f65fc7ef71ca814d859b30c
Priyanshu Kumar <priyanshudoingcoding@gmail.com>
2026-06-30 20:17:26 +0000

    Create src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+ASTD Auto Place - Educational Demonstration Package.
+
+This package demonstrates screen automation patterns for tower defense
+games using Python, OpenCV, PyAutoGUI, and keyboard.
+"""
+
+__version__ = "1.0.0"
+__author__ = "ASTD Auto Place Team"
